75e4fff423aca1c914fa850a78670465e74248f6
Marcio Silva <marcioadsworks@gmail.com>
2019-10-13 14:27:37 -0300

    created produto class and many to many association

--- src/main/java/com/marciosilva/cursomc/domain/Produto.java
+++ src/main/java/com/marciosilva/cursomc/domain/Produto.java
@@ -8,28 +8,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import org.hibernate.annotations.ManyToAny;
+
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private Double price;
 	
+	@ManyToMany
+	@JoinTable(name= "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name="produto_id"),
+			inverseJoinColumns = @JoinColumn(name="categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-
-	public Categoria() {
+	public Produto() {
 	}
 
-	public Categoria(Integer id, String name) {
+	public Produto(Integer id, String name, Double price) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
 
 	public Integer getId() {
@@ -48,14 +58,22 @@ public class Categoria implements Serializable{
 		this.name = name;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPrice(Double price) {
+		this.price = price;
 	}
-	
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -72,7 +90,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -80,8 +98,8 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-
+	
 	
 
-	
+
 }
