0a82b0e6549c44172967f33c80bacfc89be8adaf
Marcio Silva <marcioadsworks@gmail.com>
2019-10-17 22:03:05 -0300

    inserting a new Categoria with POST method

--- src/main/java/com/marciosilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/marciosilva/cursomc/services/CategoriaService.java
@@ -19,7 +19,11 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow( ()-> new ObjectNotFoundException("Objeto nao encontado! Id: "+ id 
 				+", Tipo: "+ Categoria.class.getName()));
-		
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
 
