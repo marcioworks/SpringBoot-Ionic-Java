fdbccd0b943547af28daaead2b59aa981615d8df
Marcio Silva <marcioadsworks@gmail.com>
2019-10-23 22:03:26 -0300

    validation with validation beans

--- src/main/java/com/marciosilva/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/marciosilva/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package com.marciosilva.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.marciosilva.cursomc.domain.Categoria;
 
 public class CategoriaDTO  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	@NotEmpty(message = "O campo Nao pode ser vazio")
+	@Length(min = 5,max = 80, message = "o campo deve ter no minimo 5 e no maximo 80 caracteres!")
 	private String name;
 	
 	public CategoriaDTO() {
--- src/main/java/com/marciosilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/marciosilva/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marciosilva.cursomc.domain.Categoria;
+import com.marciosilva.cursomc.dto.CategoriaDTO;
 import com.marciosilva.cursomc.repositories.CategoriaRepository;
 import com.marciosilva.cursomc.services.exception.DataViolationException;
 import com.marciosilva.cursomc.services.exception.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page,Integer linesPerPage,String orderBy,String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO obj) {
+		return new Categoria (obj.getId(),obj.getName());
 	}
 }
