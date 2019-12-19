9bb06ef9b2e400ff98dbdbc930343130c71444b3
Marcio Silva <marcioadsworks@gmail.com>
2019-12-18 22:16:17 -0300

    configuração inicial spring security

--- src/main/java/com/marciosilva/cursomc/services/SmtpMailService.java
+++ src/main/java/com/marciosilva/cursomc/services/SmtpMailService.java
@@ -5,9 +5,11 @@ import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+
 
 public class SmtpMailService extends AbstractEmailService{
 
