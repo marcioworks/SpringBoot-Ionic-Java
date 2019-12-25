c926b896bd59a591a9bc3a5b0fd5a13b19129c15
Marcio Silva <marcioadsworks@gmail.com>
2019-12-25 13:26:08 -0300

    Authorization only for especific profile

--- src/main/java/com/marciosilva/cursomc/config/SecurityConfig.java
+++ src/main/java/com/marciosilva/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,61 +23,66 @@ import com.marciosilva.cursomc.security.JWTAuthenticationFilter;
 import com.marciosilva.cursomc.security.JWTAuthorizationFilter;
 import com.marciosilva.cursomc.security.JWTUtil;
 
-
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private JWTUtil jwtUtil;
-	
+
 	@Autowired
 	private Environment env;
-	
-public static final String[] PUBLIC_MATCHERS = {
-			
+
+	public static final String[] PUBLIC_MATCHERS = {
+
 			"/h2-console/**",
-			
+
 	};
 	public static final String[] PUBLIC_MATCHERS_GET = {
-			
-			"/produtos/**",
-			"/categorias/**"
-			
+
+			"/produtos/**", "/categorias/**"
+
 	};
-	
+
+	public static final String[] PUBLIC_MATCHERS_POST = {
+
+			"/clientes/**"
+
+	};
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		
-		if(Arrays.asList(env.getActiveProfiles()).contains("dev")) {
+	protected void configure(HttpSecurity http) throws Exception {
+
+		if (Arrays.asList(env.getActiveProfiles()).contains("dev")) {
 			http.headers().frameOptions().disable();
 		}
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
+				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
+				.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		
+
 	}
-	
+
 	@Override
-	public void configure(AuthenticationManagerBuilder auth)throws Exception{
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-	
+
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
