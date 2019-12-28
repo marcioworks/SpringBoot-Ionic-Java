137aa86b6c4530d6a860478089a3da0ea80c7bfe
Marcio Silva <marcioadsworks@gmail.com>
2019-12-28 14:48:18 -0300

    send files to aws by endPoint

--- src/main/java/com/marciosilva/cursomc/CursomcApplication.java
+++ src/main/java/com/marciosilva/cursomc/CursomcApplication.java
@@ -1,42 +1,13 @@
 package com.marciosilva.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.marciosilva.cursomc.domain.Categoria;
-import com.marciosilva.cursomc.domain.Cidade;
-import com.marciosilva.cursomc.domain.Cliente;
-import com.marciosilva.cursomc.domain.Endereco;
-import com.marciosilva.cursomc.domain.Estado;
-import com.marciosilva.cursomc.domain.ItemPedido;
-import com.marciosilva.cursomc.domain.Pagamento;
-import com.marciosilva.cursomc.domain.PagamentoComBoleto;
-import com.marciosilva.cursomc.domain.PagamentoComCartao;
-import com.marciosilva.cursomc.domain.Pedido;
-import com.marciosilva.cursomc.domain.Produto;
-import com.marciosilva.cursomc.domain.enumns.EstadoPagamento;
-import com.marciosilva.cursomc.domain.enumns.TipoCliente;
-import com.marciosilva.cursomc.repositories.CategoriaRepository;
-import com.marciosilva.cursomc.repositories.CidadeRepository;
-import com.marciosilva.cursomc.repositories.ClienteRepository;
-import com.marciosilva.cursomc.repositories.EnderecoRepository;
-import com.marciosilva.cursomc.repositories.EstadoRepository;
-import com.marciosilva.cursomc.repositories.ItemPedidoRepository;
-import com.marciosilva.cursomc.repositories.PagamentoRepository;
-import com.marciosilva.cursomc.repositories.PedidoRepository;
-import com.marciosilva.cursomc.repositories.ProdutoRepository;
-import com.marciosilva.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service service;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -44,7 +15,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		service.uploadFile("C:\\temp\\Marcio.jpg");
+	
 	}
 
 }
--- src/main/java/com/marciosilva/cursomc/services/S3Service.java
+++ src/main/java/com/marciosilva/cursomc/services/S3Service.java
@@ -1,43 +1,57 @@
 package com.marciosilva.cursomc.services;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Service
 public class S3Service {
-	
+
 	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
 	@Autowired
 	private AmazonS3 s3Client;
-	
+
 	@Value("${s3.bucket}")
 	private String bucketName;
 
-	public void uploadFile(String uploadFilePath) {
-		
+	public URI uploadFile(MultipartFile multPartFile) {
+
 		try {
-			File file = new File(uploadFilePath);
-			LOG.info("Iniciando Upload");
-			s3Client.putObject(new PutObjectRequest(bucketName, "Teste.jpg", file));
-			LOG.info("Upload FInalizado");
+			String fileName = multPartFile.getOriginalFilename();
+			InputStream is = multPartFile.getInputStream();
+			String contentType = multPartFile.getContentType();
+			return uploadFile(is, fileName, contentType);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro de IO: "+ e.getMessage());
 		}
-		catch(AmazonServiceException e) {
-			LOG.info("AmazonServiceException: "+ e.getErrorMessage());
-			LOG.info("Status code: "+e.getErrorCode());
-		}
-		catch(AmazonClientException e) {
-			LOG.info("AmazonClientException: "+e.getMessage());
-		}
+
 	}
-	
+
+	public URI uploadFile(InputStream is, String fileName, String contentType) {
+		try {
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
+
+			LOG.info("Iniciando Upload");
+			s3Client.putObject(bucketName, fileName, is, meta);
+			LOG.info("Upload FInalizado");
+
+			return s3Client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("erro ao converter arquivo");
+		}
+
+	}
+
 }
